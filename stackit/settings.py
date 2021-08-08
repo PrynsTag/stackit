692592b02a32395950e22d388ed1d3ab42d2fab8
PrynsTag <princevelasco16@gmail.com>
2021-08-08 10:48:10 +0800

    feat: [common] Add Home

--- stackit/settings.py
+++ stackit/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "stackit.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "public"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
