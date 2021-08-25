771e5c39e87a818a15ce5ecef935335450f39f3d
PrynsTag <princevelasco16@gmail.com>
2021-08-25 21:19:42 +0800

    chore: [common] Remove Unused Dir

--- stackit/settings.py
+++ stackit/settings.py
@@ -92,7 +92,7 @@ ROOT_URLCONF = "stackit.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "public", BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
